ee30d7669734bcd42f5ad29abc0fc7468e3556fe
David-LBautista <196w0007@itszongolica.edu.mx>
2020-12-20 01:15:28 -0600

    api connection and getting data

--- js/app.js
+++ js/app.js
@@ -20,15 +20,57 @@ function validarFormulario(e) {
         mostrarAlerta('Agrega un termino de busqueda');
         return;
     }
+
+    buscarImagenes(termino);
 }
 
 function mostrarAlerta(mensaje) {
-    const alerta = document.createElement('p');
-    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+    const existeAlerta = document.querySelector('.bg-red-100');
 
-    alerta.innerHTML = ` 
+    if (!existeAlerta) {
+        const alerta = document.createElement('p');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = ` 
         <strong class="font-bold">Error!</strong>
         <span class="block sm:inline">${mensaje}</span>
-    `;
-    form.appendChild(alerta);
+        `;
+        form.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
+function buscarImagenes(termino) {
+    const key = '19595154-eb84613674097caafa293bf2a';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            mostrarImagenes(data.hits);
+        });
+}
+
+function mostrarImagenes(imagenes) {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+        console.log(previewURL);
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+                    <div class="p-4">
+                    <p class="font-bold">${likes} <span class="font-light">Me gusta</span> </p>
+                    </div>
+                </div>
+            </div>
+            `;
+    });
 }
